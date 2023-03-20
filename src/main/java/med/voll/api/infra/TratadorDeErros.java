6936b69b9c7c6f6c53cb6b66611f9ec64b342f3a
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2023-03-20 20:50:43 -0300

    Adicionando tratamento de erro para o status de 404

---
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -0,0 +1,17 @@
+package med.voll.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404(){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Recurso não encontrado");
+    }
+
+}
