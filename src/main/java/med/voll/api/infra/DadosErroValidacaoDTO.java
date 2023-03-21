66e4eefdebfbe97124a355496451f083e70641eb
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2023-03-20 21:08:19 -0300

    Adicionando tratamento de erro para dados inválidos

---
+++ src/main/java/med/voll/api/infra/DadosErroValidacaoDTO.java
@@ -0,0 +1,11 @@
+package med.voll.api.infra;
+
+import org.springframework.validation.FieldError;
+
+public record DadosErroValidacaoDTO(String campo, String mensagem) {
+
+    public DadosErroValidacaoDTO(FieldError err){
+        this(err.getField(), err.getDefaultMessage());
+    }
+
+}
--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -3,8 +3,12 @@ package med.voll.api.infra;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class TratadorDeErros {
@@ -12,6 +16,12 @@ public class TratadorDeErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404(){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Recurso não encontrado");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex){
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacaoDTO::new).toList());
     }
 
 }
