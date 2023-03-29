68e0c904e60582ef869fdd4cf7e4b780d724b983
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2023-03-29 19:33:44 -0300

    Concluindo geracao de token JWT

--- src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -2,6 +2,8 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.DadosAutenticacao;
+import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,13 +19,15 @@ public class AutenticacaoController {
 
     @Autowired
     private AuthenticationManager manager;
+    @Autowired
+    private TokenService service;
 
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
         var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var auth = manager.authenticate(token);
 
-        return ResponseEntity.ok();
+        return ResponseEntity.ok(service.gerarToken((Usuario) auth.getPrincipal()));
     }
 
 }
---
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -0,0 +1,35 @@
+package med.voll.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.voll.api.domain.usuario.Usuario;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    public String gerarToken(Usuario usuario){
+        try{
+            var alg = Algorithm.HMAC256("12345678");
+            return JWT.create()
+                    .withIssuer("API-Voll.med")
+                    .withSubject(usuario.getLogin())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(alg);
+        }catch (JWTCreationException e){
+            throw new RuntimeException("Erro ao gerar token JWT", e);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
