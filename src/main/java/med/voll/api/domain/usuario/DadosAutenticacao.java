e68efdae29ab3391362cfef8fb5794bd1a27d50a
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2023-03-29 13:49:14 -0300

    Concluindo processo de login

---
+++ src/main/java/med/voll/api/domain/usuario/DadosAutenticacao.java
@@ -0,0 +1,7 @@
+package med.voll.api.domain.usuario;
+
+public record DadosAutenticacao(
+        String login,
+        String senha
+) {
+}
--- src/main/java/med/voll/api/infra/exception/DadosErroValidacaoDTO.java
+++ src/main/java/med/voll/api/infra/exception/DadosErroValidacaoDTO.java
@@ -1,4 +1,4 @@
-package med.voll.api.infra;
+package med.voll.api.infra.exception;
 
 import org.springframework.validation.FieldError;
 
