fc543f1e6372be43b20e9ee1a5be554b8531d7a0
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2023-04-02 12:43:28 -0300

    Adicionando validacao do horario de funcionamento da clinica e validacao do horario de antecedencia minimo para agendamento da consulta

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
@@ -0,0 +1,21 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.infra.exception.ValidacaoException;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class ValidadorHorarioAntecedencia {
+
+    public void validarHorario(DadosAgendamentoConsulta dados){
+        var dataConsulta = dados.data();
+        var horarioAtual = LocalDateTime.now();
+        var diferencaMinutos = Duration.between(horarioAtual, dataConsulta).toMinutes();
+
+        if(diferencaMinutos < 30){
+            throw new ValidacaoException("Consulta deve ser agendada com antecedência minima de 30 minutos");
+        }
+    }
+
+}
---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
@@ -0,0 +1,21 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.infra.exception.ValidacaoException;
+
+import java.time.DayOfWeek;
+
+public class ValidadorHorarioFuncionamentoClinica {
+
+    public void validarHorario(DadosAgendamentoConsulta dados){
+        var dataConsulta = dados.data();
+        var domingo = dataConsulta.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+        var antesAberturaClinica = dataConsulta.getHour() < 7;
+        var depoisEncerramentoClinica = dataConsulta.getHour() > 18;
+
+        if(domingo || antesAberturaClinica || depoisEncerramentoClinica){
+            throw new ValidacaoException("Consulta fora do horario de funcionamento da clinica");
+        }
+    }
+
+}
